35890fe5e2b74cfda187e1dc312cb6f7ae14e8be
Raul Naupari <raulnq@gmail.com>
2025-11-09 22:20:12 -0500

    feat: add readme.md file

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,11 +1,11 @@
-import jseslint from "@eslint/js";
-import tseslint from "typescript-eslint";
-import { defineConfig, globalIgnores } from "eslint/config";
-import prettierConfig from "eslint-config-prettier";
+import jseslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig(
-  globalIgnores(["dist/**/*"]),
+  globalIgnores(['dist/**/*']),
   jseslint.configs.recommended,
   tseslint.configs.recommended,
-  prettierConfig,
+  prettierConfig
 );
